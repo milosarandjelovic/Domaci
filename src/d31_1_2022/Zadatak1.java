51dbc05b6a4090a61a94b7543bf967bcdd55ed45
Milos Arandjelovic <milos.kerber@gmail.com>
2022-02-02 22:15:42 +0100

    Prvi (31.1.) doradjen

--- src/d31_1_2022/Zadatak1.java
+++ src/d31_1_2022/Zadatak1.java
@@ -1,5 +1,7 @@
 package d31_1_2022;
 
+import java.util.List;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,31 +11,27 @@ import org.openqa.selenium.support.ui.Sleeper;
 public class Zadatak1 {
 
 	public static void main(String[] args) throws InterruptedException {
-		System.setProperty("webdriver.chrome.driver", "driver-lib/chromedriver.exe");
-		WebDriver driver = new ChromeDriver();
-
-		driver.manage().window().maximize();
 		
-		driver.get("https://s.bootsnipp.com/iframe/Dq2X");
-		
-		for (int i = 4; i >=0; i--) {
-			driver.findElement(By.xpath("//*[(@class= 'col-md-12')]//button")).click();
-
-			boolean elementDeleted;
-
-			try {
-				WebElement e = driver.findElement(By.xpath("//div[@class='col-md-12']/div/button"));
-				elementDeleted = true;
-			} catch (Exception e2) {
-				elementDeleted = false;
+			System.setProperty("webdriver.chrome.driver",
+					"driver-lib/chromedriver.exe");
+			WebDriver driver = new ChromeDriver();
+			
+			driver.navigate().to("https://s.bootsnipp.com/iframe/Dq2X");
+			
+			List<WebElement> theList = driver.findElements(By.xpath("//div[@class='col-md-12']//button"));
+			
+			
+			for (int i = 1; i <= theList.size(); i++) {
+				driver.findElement(By.xpath("//div[@class='col-md-12']/div/button")).click();
+				
+				List<WebElement> newList = driver.findElements(By.xpath("//div[@class='col-md-12']//button"));
+				
+				if(theList.size()==i+newList.size()) {
+					System.out.println("Element deleted.");
+				} else {
+					System.out.println("Element not deleted.");
+				}
 			}
-
-			if (elementDeleted) {
-				System.out.println("Element is there.");
-			} else {
-				System.out.println("Element is not there.");
-			}	
-		}
 		
 		
 		
